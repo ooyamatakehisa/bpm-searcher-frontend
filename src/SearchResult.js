7540b5f1552cb331d7c46e6ec614cf35b526d600
ooyamatakehisa <shinobu_take_kobukuro5296@yahoo.co.jp>
2021-08-04 13:29:38 +0900

    検索結果画面を整える

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -12,33 +13,41 @@ import Box from '@material-ui/core/Box';
 export default function SearchBox(props) {
 
     return (
-        <Box px={10}>
-            <TableContainer component={Paper}>
-                <Table  aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>SONG</TableCell>
-                            <TableCell></TableCell>
-                            <TableCell>ARTIST</TableCell>
-                            <TableCell>ALBUM</TableCell>
-                            <TableCell>BPM</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {props.result.map((row) => (
-                        <TableRow key={row.song_name}>
-                            <TableCell>
-                                <img src={row.image_url} height="100"/>
-                            </TableCell>
-                            <TableCell>{row.song_name}</TableCell>
-                            <TableCell>{row.artist}</TableCell>
-                            <TableCell>{row.album_name}</TableCell>
-                            <TableCell>{row.bpm}</TableCell>
-                        </TableRow>
-                    ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+        <Box width={1}  display="flex" justifyContent="center">
+            <Box width={0.85} justifyContent="center">
+                <TableContainer component={Paper}>
+                    <Table  aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">
+                                    <div>SONG</div>
+                                    {/* <div>ARTIST</div> */}
+                                </TableCell>
+                                <TableCell></TableCell>
+                                {/* <TableCell>ARTIST</TableCell> */}
+                                {/* <TableCell>ALBUM</TableCell> */}
+                                <TableCell>BPM</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {props.result.map((row) => (
+                            <TableRow key={row.song_name}>
+                                <TableCell align="center">
+                                    <img src={row.image_url} height="100"/>
+                                </TableCell>
+                                <TableCell>
+                                    <Typography>{row.song_name}</Typography>
+                                    <div>{row.artist}</div>
+                                </TableCell>
+                                {/* <TableCell>{row.artist}</TableCell> */}
+                                {/* <TableCell>{row.album_name}</TableCell> */}
+                                <TableCell><Typography>{row.bpm}</Typography></TableCell>
+                            </TableRow>
+                        ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
         </Box>
     );
 }
