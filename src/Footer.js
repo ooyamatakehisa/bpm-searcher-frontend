5e495e89c5bf5766af255b03903a2e011dd90d9e
ooyamatakehisa <ooyamatakehisa@gmail.com>
2021-12-05 15:07:43 +0900

    fix footer to bottom(#19)

--- src/Footer.js
+++ src/Footer.js
@@ -20,6 +20,9 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     textAlign: "center",
   },
+  footerMain: {
+    bottom: 0,
+  },
 }));
 
 export default function Footer() {
@@ -29,47 +32,49 @@ export default function Footer() {
     window.open(url, "_blank");
   }
   return (
-    <Box color="primary" className={classes.footer}>
-      <Box p={10}>
-        <Grid
-          container
-          direction="column"
-          alignItems="center"
-          alignContent="center"
-          spacing={2}
-        >
-          <Grid item xs={12}>
-            <img src={`${BASE_URL}/favicon.svg`} height="25" alt="logo" />
+    <div className={classes.footerMain}>
+      <Box color="primary" className={classes.footer}>
+        <Box p={10}>
+          <Grid
+            container
+            direction="column"
+            alignItems="center"
+            alignContent="center"
+            spacing={2}
+          >
+            <Grid item xs={12}>
+              <img src={`${BASE_URL}/favicon.svg`} height="25" alt="logo" />
+            </Grid>
+            <Grid item xs={12}>
+              <StyledEngineProvider injectFirst>
+                <ThemeProvider theme={themeLogo}>
+                  <Typography
+                    variant="h5"
+                    noWrap
+                    color="primary"
+                    display="inline"
+                  >
+                    {APP_NAME}
+                  </Typography>
+                </ThemeProvider>
+              </StyledEngineProvider>
+            </Grid>
+            <Grid item xs={12}>
+              <IconButton
+                aria-label="show 17 new notifications"
+                color="inherit"
+                onClick={onClickGithub}
+                size="large"
+              >
+                <GitHubIcon />
+              </IconButton>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <StyledEngineProvider injectFirst>
-              <ThemeProvider theme={themeLogo}>
-                <Typography
-                  variant="h5"
-                  noWrap
-                  color="primary"
-                  display="inline"
-                >
-                  {APP_NAME}
-                </Typography>
-              </ThemeProvider>
-            </StyledEngineProvider>
-          </Grid>
-          <Grid item xs={12}>
-            <IconButton
-              aria-label="show 17 new notifications"
-              color="inherit"
-              onClick={onClickGithub}
-              size="large"
-            >
-              <GitHubIcon />
-            </IconButton>
-          </Grid>
-        </Grid>
-        <Box p={2}>
-          <Typography>© Takehisa Oyama. All rights reserved.</Typography>
+          <Box p={2}>
+            <Typography>© Takehisa Oyama. All rights reserved.</Typography>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </div>
   );
-};
+}
