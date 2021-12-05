90a97fcd48abfaf733e39064080cc01bb21681fd
ooyamatakehisa <ooyamatakehisa@gmail.com>
2021-12-05 15:38:42 +0900

    add loading icon to playlist pages(#21)

--- src/DraggableTable.js
+++ src/DraggableTable.js
@@ -21,6 +21,7 @@ import axios from "axios";
 
 import Snackbar from "./MySnackbar";
 import { API_BASE_URL } from "./constant";
+import Loading from "./Loading";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -34,6 +35,7 @@ export default function DraggableTable({ isSignedIn, setSignInDialogOpen }) {
   // cache the items provided via props in state for purposes of this demo
   const [playlistTracks, setplaylistTracks] = useState([]);
   const [playlistInfo, setPlaylistInfo] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   const [snackbarOpen, setSnackbarOpen] = React.useState(false);
   const [snackbarMessage, setSnackbarMessage] = React.useState("");
   const history = useHistory();
@@ -56,6 +58,7 @@ export default function DraggableTable({ isSignedIn, setSignInDialogOpen }) {
           .then((data) => {
             setplaylistTracks(data.data.playlist_tracks);
             setPlaylistInfo(data.data.playlist_info);
+            setIsLoading(false);
           })
           .catch((err) => console.log(err));
       } else {
@@ -157,167 +160,175 @@ export default function DraggableTable({ isSignedIn, setSignInDialogOpen }) {
 
   return (
     <Box width={1} display="flex" justifyContent="center">
-      <Box width={0.8} justifyContent="center" p={5}>
-        <Grid container spacing={8}>
-          <Grid item xs={12} sm={3} align="center">
-            <Grid container direction="column" spacing={4}>
-              <Grid item xs={12} sm={6} align="center">
-                <Box width="100%">
-                  <img
-                    src={playlistInfo.image_url}
-                    width="100%"
-                    style={{ maxWidth: "300px" }}
-                    alt={playlistInfo.name}
-                  />
-                </Box>
-              </Grid>
-              <Grid item xs={12} sm={6} className={classes.root}>
-                <Typography variant="h4">{playlistInfo.name}</Typography>
-                <Typography variant="h5" color="textSecondary">
-                  {playlistInfo.desc}
-                </Typography>
-                <Box py={2}>
-                  <Divider />
-                </Box>
-                <Typography variant="body2" color="textSecondary">
-                  {playlistInfo.num_tracks} songs
-                  <br />
-                  Created At {datetime2date(playlistInfo.created_at)}
-                  <br />
-                  Updated At {datetime2date(playlistInfo.updated_at)}
-                </Typography>
+      {isLoading && <Loading />}
+      {!isLoading && (
+        <Box width={0.8} justifyContent="center" p={5}>
+          <Grid container spacing={8}>
+            <Grid item xs={12} sm={3} align="center">
+              <Grid container direction="column" spacing={4}>
+                <Grid item xs={12} sm={6} align="center">
+                  <Box width="100%">
+                    <img
+                      src={playlistInfo.image_url}
+                      width="100%"
+                      style={{ maxWidth: "300px" }}
+                      alt={playlistInfo.name}
+                    />
+                  </Box>
+                </Grid>
+                <Grid item xs={12} sm={6} className={classes.root}>
+                  <Typography variant="h4">{playlistInfo.name}</Typography>
+                  <Typography variant="h5" color="textSecondary">
+                    {playlistInfo.desc}
+                  </Typography>
+                  <Box py={2}>
+                    <Divider />
+                  </Box>
+                  <Typography variant="body2" color="textSecondary">
+                    {playlistInfo.num_tracks} songs
+                    <br />
+                    Created At {datetime2date(playlistInfo.created_at)}
+                    <br />
+                    Updated At {datetime2date(playlistInfo.updated_at)}
+                  </Typography>
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
-          <Grid item xs={12} sm={9} align="center">
-            <TableContainer>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell align="center"></TableCell>
-                    <TableCell align="center">
-                      <Typography variant="subtitle1" gutterBottom>
-                        SONG
-                      </Typography>
-                    </TableCell>
-                    <TableCell align="center">
-                      <Typography variant="subtitle1" gutterBottom></Typography>
-                    </TableCell>
-                    <TableCell></TableCell>
-                    <TableCell width="15%" align="center">
-                      <Typography variant="subtitle1" gutterBottom>
-                        BPM
-                      </Typography>
-                    </TableCell>
-                    <TableCell width="15%" align="center">
-                      <Typography variant="subtitle1" gutterBottom>
-                        DELETE
-                      </Typography>
-                    </TableCell>
-                  </TableRow>
-                </TableHead>
-                <DragDropContext onDragEnd={handleDragEnd}>
-                  <Droppable droppableId="droppable" direction="vertical">
-                    {(droppableProvided) => (
-                      <TableBody
-                        ref={droppableProvided.innerRef}
-                        {...droppableProvided.droppableProps}
-                      >
-                        {playlistTracks.map((plyalistTrack, index) => (
-                          <Draggable
-                            key={plyalistTrack.id}
-                            draggableId={plyalistTrack.id}
-                            index={index}
-                          >
-                            {(draggableProvided, snapshot) => {
-                              return (
-                                <TableRow
-                                  hover={true}
-                                  ref={draggableProvided.innerRef}
-                                  {...draggableProvided.draggableProps}
-                                  style={{
-                                    ...draggableProvided.draggableProps.style,
-                                    cursor: "pointer",
-                                  }}
-                                >
-                                  <TableCell align="center">
-                                    <div {...draggableProvided.dragHandleProps}>
-                                      <Reorder />
-                                    </div>
-                                  </TableCell>
-                                  <TableCell
-                                    align="center"
-                                    onClick={() =>
-                                      handleLink(
-                                        plyalistTrack.track.spotify_id,
-                                        plyalistTrack.track
-                                      )
-                                    }
+            <Grid item xs={12} sm={9} align="center">
+              <TableContainer>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell align="center"></TableCell>
+                      <TableCell align="center">
+                        <Typography variant="subtitle1" gutterBottom>
+                          SONG
+                        </Typography>
+                      </TableCell>
+                      <TableCell align="center">
+                        <Typography
+                          variant="subtitle1"
+                          gutterBottom
+                        ></Typography>
+                      </TableCell>
+                      <TableCell></TableCell>
+                      <TableCell width="15%" align="center">
+                        <Typography variant="subtitle1" gutterBottom>
+                          BPM
+                        </Typography>
+                      </TableCell>
+                      <TableCell width="15%" align="center">
+                        <Typography variant="subtitle1" gutterBottom>
+                          DELETE
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <DragDropContext onDragEnd={handleDragEnd}>
+                    <Droppable droppableId="droppable" direction="vertical">
+                      {(droppableProvided) => (
+                        <TableBody
+                          ref={droppableProvided.innerRef}
+                          {...droppableProvided.droppableProps}
+                        >
+                          {playlistTracks.map((plyalistTrack, index) => (
+                            <Draggable
+                              key={plyalistTrack.id}
+                              draggableId={plyalistTrack.id}
+                              index={index}
+                            >
+                              {(draggableProvided, snapshot) => {
+                                return (
+                                  <TableRow
+                                    hover={true}
+                                    ref={draggableProvided.innerRef}
+                                    {...draggableProvided.draggableProps}
+                                    style={{
+                                      ...draggableProvided.draggableProps.style,
+                                      cursor: "pointer",
+                                    }}
                                   >
-                                    <img
-                                      src={plyalistTrack.track.image_url}
-                                      height="100"
-                                      alt={plyalistTrack.track.song_name}
-                                    />
-                                  </TableCell>
-                                  <TableCell
-                                    align="center"
-                                    onClick={() =>
-                                      handleLink(
-                                        plyalistTrack.track.spotify_id,
-                                        plyalistTrack.track
-                                      )
-                                    }
-                                  >
-                                    <Typography variant="h6" gutterBottom>
-                                      {index + 1}
-                                    </Typography>
-                                  </TableCell>
-                                  <TableCell
-                                    align="left"
-                                    onClick={() =>
-                                      handleLink(
-                                        plyalistTrack.track.spotify_id,
-                                        plyalistTrack.track
-                                      )
-                                    }
-                                  >
-                                    <Typography variant="h6">
-                                      <Box fontWeight="fontWeightMedium">
-                                        {plyalistTrack.track.song_name}
-                                      </Box>
-                                    </Typography>
-                                    <div>{plyalistTrack.track.artist}</div>
-                                  </TableCell>
-                                  <TableCell align="center">
-                                    <Typography variant="h5" gutterBottom>
-                                      {Math.round(plyalistTrack.track.bpm)}
-                                    </Typography>
-                                  </TableCell>
-                                  <TableCell align="center">
-                                    <IconButton
+                                    <TableCell align="center">
+                                      <div
+                                        {...draggableProvided.dragHandleProps}
+                                      >
+                                        <Reorder />
+                                      </div>
+                                    </TableCell>
+                                    <TableCell
+                                      align="center"
                                       onClick={() =>
-                                        deleteTrack(plyalistTrack, index)
+                                        handleLink(
+                                          plyalistTrack.track.spotify_id,
+                                          plyalistTrack.track
+                                        )
                                       }
                                     >
-                                      <DeleteSweep />
-                                    </IconButton>
-                                  </TableCell>
-                                </TableRow>
-                              );
-                            }}
-                          </Draggable>
-                        ))}
-                        {droppableProvided.placeholder}
-                      </TableBody>
-                    )}
-                  </Droppable>
-                </DragDropContext>
-              </Table>
-            </TableContainer>
+                                      <img
+                                        src={plyalistTrack.track.image_url}
+                                        height="100"
+                                        alt={plyalistTrack.track.song_name}
+                                      />
+                                    </TableCell>
+                                    <TableCell
+                                      align="center"
+                                      onClick={() =>
+                                        handleLink(
+                                          plyalistTrack.track.spotify_id,
+                                          plyalistTrack.track
+                                        )
+                                      }
+                                    >
+                                      <Typography variant="h6" gutterBottom>
+                                        {index + 1}
+                                      </Typography>
+                                    </TableCell>
+                                    <TableCell
+                                      align="left"
+                                      onClick={() =>
+                                        handleLink(
+                                          plyalistTrack.track.spotify_id,
+                                          plyalistTrack.track
+                                        )
+                                      }
+                                    >
+                                      <Typography variant="h6">
+                                        <Box fontWeight="fontWeightMedium">
+                                          {plyalistTrack.track.song_name}
+                                        </Box>
+                                      </Typography>
+                                      <div>{plyalistTrack.track.artist}</div>
+                                    </TableCell>
+                                    <TableCell align="center">
+                                      <Typography variant="h5" gutterBottom>
+                                        {Math.round(plyalistTrack.track.bpm)}
+                                      </Typography>
+                                    </TableCell>
+                                    <TableCell align="center">
+                                      <IconButton
+                                        onClick={() =>
+                                          deleteTrack(plyalistTrack, index)
+                                        }
+                                      >
+                                        <DeleteSweep />
+                                      </IconButton>
+                                    </TableCell>
+                                  </TableRow>
+                                );
+                              }}
+                            </Draggable>
+                          ))}
+                          {droppableProvided.placeholder}
+                        </TableBody>
+                      )}
+                    </Droppable>
+                  </DragDropContext>
+                </Table>
+              </TableContainer>
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      )}
       <Snackbar
         snackbarOpen={snackbarOpen}
         setSnackbarOpen={setSnackbarOpen}
--- src/Playlist.js
+++ src/Playlist.js
@@ -23,10 +23,11 @@ import { DeleteSweep } from "@mui/icons-material";
 
 import Snackbar from "./MySnackbar";
 import { API_BASE_URL } from "./constant";
-// import Waiting from "./Waiting";
+import Loading from "./Loading";
 
 export default function Playlist({ isSignedIn, setSignInDialogOpen }) {
   const [playlistInfos, setPlaylistInfos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [snackbarOpen, setSnackbarOpen] = React.useState(false);
   const [snackbarMessage, setSnackbarMessage] = React.useState("");
   const [playlistIdxForDeletion, setPlaylistIdxForDeletion] =
@@ -48,6 +49,7 @@ export default function Playlist({ isSignedIn, setSignInDialogOpen }) {
           .get(`${API_BASE_URL}/user/${user.uid}/playlist`, config)
           .then((data) => {
             setPlaylistInfos(data.data);
+            setIsLoading(false);
           })
           .catch((err) => console.log(err));
       } else {
@@ -98,72 +100,75 @@ export default function Playlist({ isSignedIn, setSignInDialogOpen }) {
 
   return (
     <Box width={1} display="flex" justifyContent="center">
-      <Box width={0.8} py={10} justifyContent="center">
-        <Grid
-          container
-          direction="row"
-          alignItems="center"
-          alignContent="center"
-          spacing={2}
-          width={1}
-        >
-          {playlistInfos.map((row, index) => (
-            <Grid item xs={3} key={row.id}>
-              <Card
-                onClick={onClickPlaylist(row)}
-                style={{ cursor: "pointer" }}
-              >
-                <CardHeader
-                  action={
-                    <IconButton
-                      aria-label="settings"
-                      onClick={openDeleteDialog(index)}
-                    >
-                      <DeleteSweep />
-                    </IconButton>
-                  }
-                  title={row.name}
-                  subheader={`${row.num_tracks} songs`}
-                />
-                {/* {row.image_url == null && <Divider/>} */}
-                {row.image_url != null && (
-                  <CardMedia
-                    component="img"
-                    // height="194"
-                    image={row.image_url}
-                    alt={row.name}
+      {isLoading && <Loading />}
+      {!isLoading && (
+        <Box width={0.8} py={10} justifyContent="center">
+          <Grid
+            container
+            direction="row"
+            alignItems="center"
+            alignContent="center"
+            spacing={2}
+            width={1}
+          >
+            {playlistInfos.map((row, index) => (
+              <Grid item xs={3} key={row.id}>
+                <Card
+                  onClick={onClickPlaylist(row)}
+                  style={{ cursor: "pointer" }}
+                >
+                  <CardHeader
+                    action={
+                      <IconButton
+                        aria-label="settings"
+                        onClick={openDeleteDialog(index)}
+                      >
+                        <DeleteSweep />
+                      </IconButton>
+                    }
+                    title={row.name}
+                    subheader={`${row.num_tracks} songs`}
                   />
-                )}
-                <CardContent>
-                  <Typography variant="body1" color="text.secondary">
-                    {row.desc}
-                  </Typography>
-                  <Divider />
+                  {/* {row.image_url == null && <Divider/>} */}
+                  {row.image_url != null && (
+                    <CardMedia
+                      component="img"
+                      // height="194"
+                      image={row.image_url}
+                      alt={row.name}
+                    />
+                  )}
+                  <CardContent>
+                    <Typography variant="body1" color="text.secondary">
+                      {row.desc}
+                    </Typography>
+                    <Divider />
 
-                  <Typography variant="body2" color="text.secondary">
-                    <br />
-                    Created At
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {row.created_at}
-                  </Typography>
-                </CardContent>
-                {/* <CardActions>
+                    <Typography variant="body2" color="text.secondary">
+                      <br />
+                      Created At
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {row.created_at}
+                    </Typography>
+                  </CardContent>
+                  {/* <CardActions>
                   <Button size="small">Share</Button>
                   <Button size="small">Learn More</Button>
                 </CardActions> */}
-              </Card>
-            </Grid>
-          ))}
-          {/* <Grid item xs={3}></Grid>
+                </Card>
+              </Grid>
+            ))}
+            {/* <Grid item xs={3}></Grid>
         <Grid item xs={3}></Grid>
         <Grid item xs={3}></Grid> */}
-          {/* {playlist.length !== 0 && (
+            {/* {playlist.length !== 0 && (
           <DraggableTable playlist={playlist} playlistInfo={playlistInfo} />
           )}
         {playlist.length == 0 && <Waiting />} */}
-        </Grid>
-      </Box>
+          </Grid>
+        </Box>
+      )}
       <Snackbar
         snackbarOpen={snackbarOpen}
         setSnackbarOpen={setSnackbarOpen}
