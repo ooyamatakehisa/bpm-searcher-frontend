d6801dfa63de6706c80846b11d1e04bb74b07d0f
ooyamatakehisa <ooyamatakehisa@gmail.com>
2021-12-07 16:45:34 +0900

    update sign in dialog(#24)

--- src/SignInDialog.js
+++ src/SignInDialog.js
@@ -22,12 +22,16 @@ export default function SignInDialog({ signInDialogOpen, closeSignInDialog }) {
       <DialogTitle id="alert-dialog-title">Sign In / Sign Up</DialogTitle>
       <DialogContent>
         <DialogContentText id="alert-dialog-description">
-          {APP_NAME} uses the Firebase Authentication as login service and does
-          not have any user information on its server.
+          You can create playlists and save songs to it if you sign in this
+          system. You can also sync the playlists with Spotify playlists in future.
         </DialogContentText>
         <Box p={2}>
           <SignIn />
         </Box>
+        <DialogContentText>
+          {APP_NAME} uses the Firebase Authentication as login service and does
+          not have any user information on its server.
+        </DialogContentText>
       </DialogContent>
       <DialogActions>
         <Button onClick={closeSignInDialog} color="primary">
